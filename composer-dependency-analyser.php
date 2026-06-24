703b9aefc95d6bb7fa407109c040b6d4e0f32674
Jim Schmid <jim.schmid@1up.io>
2026-06-24 14:55:52 +0200

    Use PHP8.3, since some projects still depend on it

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return new Configuration()
+return (new Configuration())
     ->ignoreErrorsOnPackages(
         [
             'symfony/config',
